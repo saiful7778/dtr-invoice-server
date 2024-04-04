d8ac4c575b9889810b6509302d038e6b9785dcab
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-04-04 15:31:43 +0600

    add mongoose database conntection

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { product } from "./src/routes/product.js";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const port = process.env.PORT || 5001;
+const dbUrl = process.env.DB_CONNECT;
+(async () => {
+  try {
+    console.log("connecting...");
+    await mongoose.connect(dbUrl);
+    console.log("connected DB");
+  } catch (err) {
+    console.log("connection failed");
+    console.log(err);
+  }
+})();
 
 const app = express();
 
 app.use(
   cors({
+    origin: ["http://localhost:3000"],
     methods: ["GET", "POST", "DELETE", "PATCH"],
   })
 );
@@ -18,6 +32,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.status(200).send({ success: true, message: "Server is running" });
 });
+
+app.use("/product", product);
 
 app.listen(port, () => {
   console.log("server is running on port:", port);
